6cbece51ed8750c1ef2acbb23c71b0b12d3a1b5e
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-13 19:19:27 +0530

    Refactor. Name the condition, isDuplicate

--- library.js
+++ library.js
@@ -4,7 +4,8 @@
 
 let findUniqueElements = function(source){
   if(source.length == 0) {return source}
-  if(source[0] == source[1]) {return [source[0]] }
+  let isDuplicate = (source[0] == source[1]);
+  if(isDuplicate) {return [source[0]] }
   return source;
 };
 
