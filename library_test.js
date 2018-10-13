03b3eaca0213cccda8c349e20c95156383a2edcb
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-13 17:59:48 +0530

    Refactor. Introduce assertUnique.
    Easy way to invoke and assert on findUniqueElements

--- library_test.js
+++ library_test.js
@@ -1,6 +1,13 @@
 let assert = require('assert');
 let findUniqueElements = require('./library.js').findUniqueElements;
 
-// Empty array should return empty array
-assert.equal(findUniqueElements([]), [])
+//Unique Elements of
+const assertUnique = function(source,expected){
+  assert.equal(findUniqueElements(source),expected);
+}
+//          empty array should be an empty array
+assertUnique([],[]);
+
+//          an array of single element is an array of just that element
+//assert.equal(findUniqueElements([1]),[1]);
 
