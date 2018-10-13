fab9fc82e518894ecc318785702beaea2e4b17d7
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-13 17:25:59 +0530

    Initial commit. Starting findUniqueElements.
    Handle the case of empty array

---
+++ library.js
@@ -0,0 +1,10 @@
+/*
+ * 20. Unique - Given an array, remove duplicate elements and return an array of only unique elements.
+ */
+
+let findUniqueElements = function(){
+  return "";
+};
+
+exports.findUniqueElements = findUniqueElements;
+
---
+++ library_test.js
@@ -0,0 +1,6 @@
+let assert = require('assert');
+let findUniqueElements = require('./library.js').findUniqueElements;
+
+// Empty array should return empty array
+assert.equal(findUniqueElements([]), [])
+
